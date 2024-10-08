34f599f84d62376c6f4adeed078d4d5220b1d7fa
kalops1a <vadymduchenko@gmail.com>
2024-10-09 00:30:47 +0200

    7

--- src/server.js
+++ src/server.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const pino = require('pino-http')();
-
 const { getContacts, getContactById } = require('./controllers/contactsController');
-app.get('/contacts', getContacts);
-app.get('/contacts/:contactId', getContactById);
 
 const setupServer = () => {
   const app = express();
+
   app.use(cors());
   app.use(pino);
   app.use(express.json());
+
+  app.get('/contacts', getContacts);
+  app.get('/contacts/:contactId', getContactById);
 
   app.use((req, res) => {
     res.status(404).json({ message: 'Not found' });
