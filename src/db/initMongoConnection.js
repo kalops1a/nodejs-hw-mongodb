0755f133df4d6d93721f02d44930dcc431ce7632
kalops1a <vadymduchenko@gmail.com>
2024-10-09 19:42:21 +0200

    21

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -3,14 +3,10 @@ const mongoose = require('mongoose');
 const initMongoConnection = async () => {
   const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_URL, MONGODB_DB } = process.env;
 
-const uri = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
-
+  const uri = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
 
   try {
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(uri);
     console.log('Mongo connection successfully established!');
   } catch (error) {
     console.error('Mongo connection failed:', error.message);
