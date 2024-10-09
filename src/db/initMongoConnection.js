946166ea794604611897e581884ea02c54233e41
kalops1a <vadymduchenko@gmail.com>
2024-10-09 18:11:48 +0200

    9

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const initMongoConnection = async () => {
   const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_URL, MONGODB_DB } = process.env;
 
-  const uri = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
+ const uri = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
+
 
   try {
     await mongoose.connect(uri, {
